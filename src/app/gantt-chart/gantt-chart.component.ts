df579bbf2fc08efe3f1900a0499b79d2dab12bee
junyafujii <37508010+junyafujii@users.noreply.github.com>
2019-01-03 22:34:35 +0900

    Add files via upload

--- src/app/gantt-chart/gantt-chart.component.ts
+++ src/app/gantt-chart/gantt-chart.component.ts
@@ -8,20 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class GanttChartComponent implements OnInit {
 
   dispDates = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
+  plans = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31'];
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  hasPlan(){
-    console.log("hasPlan");    
+  /*
+    計画がある日かどうか判定
+  */
+  hasPlan()　{
     return true;
   }
 
-  isPlanEdge(items: Number){
-    console.log("aaaisPlanEdge");
-    if(items === 1){
+  /*
+    計画がある日の初日かどうか
+    塗りつぶし領域の左側を削る
+  */
+  isPlanEdgeS(items: string) {
+    if (items === '1') {
+      return true;
+    }
+  }
+
+　 /*
+    計画がある日の最終日かどうか
+    塗りつぶし領域の右側を削る
+  */
+  isPlanEdgeE(items: string) {
+    if (items === this.dispDates.length.toString() ) {
       return true;
     }
   }
